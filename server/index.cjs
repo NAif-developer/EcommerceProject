79249e5e3fc64de3ce49f9a7f46da095ffd226a0
NAif-developer <naif.mutairi2003@gmail.com>
2026-07-06 13:27:13 +0300

    Fix catch all route

--- server/index.cjs
+++ server/index.cjs
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api/products', require('./routes/products.cjs'));
 app.use(express.static(path.join(__dirname, '../app/dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../app/dist/index.html'));
 });
 
